36c2c87495c7acad0ff9da37d95cc3461c0101bb
Luis Taira <luishptaira@gmail.com>
2020-12-06 13:39:51 +0000

    IDK promise

--- src/stage.js
+++ src/stage.js
@@ -1,16 +1,13 @@
 class Stage {
 	constructor(game, stage_name, tileset='tilemap') {
 		this.map = game.make.tilemap({ key: 'boss_stage' });
-		//this.iceMap = game.make.tilemap({ key: 'boss_stage' });
 		this.ice_layer = this.map.createStaticLayer('Ice', tileset, 0, 0);
 		this.tileset = this.map.addTilesetImage(tileset);
 		this.background_layer = this.map.createStaticLayer('Background', tileset, 0, 0);
 		this.wall_layer = this.map.createStaticLayer('Walls', tileset, 0, 0);
 		this.floor_layer = this.map.createStaticLayer('Floor', tileset, 0, 0);
 		game.children.bringToTop(this.ice_layer);
-		//Dynamic loading is async and is pending a solution.
-		//game.load.json(stage_name, `/src/stages/${stage_name}_info.json`);
-		//game.load.start();
+		
 		let stage_json = game.cache.json.get(stage_name + '_info');
 		this.spawn_point = stage_json.spawn_point;
 		this.enemies = stage_json.enemies;
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 function cast_ray_into_tilemap(x0, y0, x1, y1, layer) {
 	ray = new Phaser.Geom.Line(x0, y0, x1, y1);
-	return layer.getTilesWithinShape(ray, {isNotEmpty: true})
+	return layer.getTilesWithinShape(ray, { isNotEmpty: true })
 }
 
 class Graph {
@@ -12,118 +12,123 @@ class Graph {
 		this.edgeList = [];
 		this.hasCycle = false;
 	}
-	
+
 	addVertex(v, info = {}) {
-		this.adjList.set(v, {...info, edges: []});
+		this.adjList.set(v, { ...info, edges: [] });
 		this.size++;
 	}
-	
+
 	addEdge(v1, v2, weight) {
-		this.adjList.get(v1).edges.push({edge: v2, weight: weight});
-		this.adjList.get(v2).edges.push({edge: v1, weight: weight});
-		this.edgeList.push({src: v1, dest: v2, weight: weight});
+		this.adjList.get(v1).edges.push({ edge: v2, weight: weight });
+		this.adjList.get(v2).edges.push({ edge: v1, weight: weight });
+		this.edgeList.push({ src: v1, dest: v2, weight: weight });
 		this.edges++;
 	}
-	
+
 	getVertex(v) {
 		return this.adjList.get(v);
 	}
 
 	BFSShortestPath(vRoot, vDestination) {
-		if(vRoot == vDestination) {
-			return [vDestination]
-		}
-		var originTreeEdge = new Array();
-		var destinationTreeEdge = new Array();
-		var queue = new Array();
-		var visitedNodes = new Array();
-		var run = true;
-		
-		queue.push(vRoot);
-		visitedNodes.push(vRoot);
-		
-		while(queue.length > 0 && run) {
-			var uNode = queue.shift()
-			this.adjList.get(uNode).edges.forEach(vNode => {
-				if(!visitedNodes.includes(vNode.edge) && run) {
-					originTreeEdge.unshift(uNode)
-					destinationTreeEdge.unshift(vNode.edge)
-					if(vNode.edge == vDestination || uNode == vDestination) {
+		return new Promise((resolve) => {
+
+			if (vRoot == vDestination) {
+				return [vDestination]
+			}
+			var originTreeEdge = new Array();
+			var destinationTreeEdge = new Array();
+			var queue = new Array();
+			var visitedNodes = new Array();
+			var run = true;
+
+			queue.push(vRoot);
+			visitedNodes.push(vRoot);
+
+			while (queue.length > 0 && run) {
+				var uNode = queue.shift()
+				this.adjList.get(uNode).edges.forEach(vNode => {
+					if (!visitedNodes.includes(vNode.edge) && run) {
+						originTreeEdge.unshift(uNode)
+						destinationTreeEdge.unshift(vNode.edge)
+						if (vNode.edge == vDestination || uNode == vDestination) {
+							run = false;
+						}
+						if (run) {
+							visitedNodes.push(vNode.edge)
+							queue.push(vNode.edge)
+						}
+					}
+				});
+			}
+
+			run = true;
+			var path = new Array()
+			var destination = vDestination;
+			destinationTreeEdge.forEach((node, index) => {
+				if (node == destination && run) {
+					path.unshift(node)
+					destination = originTreeEdge[index];
+					if (destination == vRoot) {
 						run = false;
 					}
-					if(run) {
-						visitedNodes.push(vNode.edge)
-						queue.push(vNode.edge)
+				}
+
+			})
+			resolve(path);
+		});
+	}
+	
+	bellmanFord(start_node, last_node) {
+		return new Promise((resolve, reject) => {
+			let distance = {};
+			let predecessor = {};
+			let path = [];
+			let target = last_node;
+
+			for (let i = 0; i < this.size; i++) {
+				distance[i] = Math.max();
+			}
+			distance[start_node] = 0;
+
+			for (let i = 1; i < this.size; i++) {
+				for (let j = 0; j < this.edges; j++) {
+					let edge = this.edgeList[j]
+					let u = edge.src;
+					let v = edge.dest;
+					let w = edge.weight;
+
+					if (distance[u] != Math.max() && distance[u] + w < distance[v]) {
+						distance[v] = distance[u] + w;
+						predecessor[v] = u;
 					}
 				}
-			});
-		}
-
-		run = true;
-		var path = new Array()
-		var destination = vDestination;
-		destinationTreeEdge.forEach((node, index) => {
-			if(node == destination && run) {
-				path.unshift(node)
-				destination = originTreeEdge[index];
-				if(destination == vRoot) {
-					run = false;
-				}
 			}
-			
-		})
-		return path[0];
-	}
 
-	bellmanFord(start_node, last_node){
-		let distance = {};
-		let predecessor = {};
-		let path = [];
-		let target = last_node;
-	
-		for(let i = 0; i < this.size; i++){
-			distance[i] = Math.max();
-		}
-		distance[start_node] = 0;
-	
-		for(let i = 1; i < this.size; i++){
-			for(let j = 0; j < this.edges; j++){
-				let edge = this.edgeList[j]
+			for (let i = 0; i < this.edges; i++) {
+				let edge = this.edgeList[i]
 				let u = edge.src;
 				let v = edge.dest;
 				let w = edge.weight;
-	
-				if(distance[u] != Math.max() && distance[u] + w < distance[v]){
-					distance[v] = distance[u] + w;
-					predecessor[v] = u;
+
+				if (distance[u] != Math.max() && distance[u] + w < distance[v]) {
+
+					this.hasCycle = true;
+				}
+
+			}
+
+			if (this.hasCycle) {
+				console.log("Ciclo negativo");
+			}
+
+			else {
+
+				while (target != null) {
+					path.unshift(target);
+					target = predecessor[target]
 				}
 			}
-		}
-	
-		for(let i = 0; i < this.edges; i ++){
-			let edge = this.edgeList[i]
-			let u = edge.src;
-			let v = edge.dest;
-			let w = edge.weight;
-			
-			if(distance[u] != Math.max() && distance[u] + w < distance[v]){
-				
-				this.hasCycle = true;
-			}
-					
-		}
-	
-		if(this.hasCycle){
-			console.log("Ciclo negativo");
-		}
-			
-		else{
-		
-			while(target != null){
-				path.unshift(target);
-				target = predecessor[target]
-			}
-		}
-		return path;
+			resolve(path);
+		});
 	}
 }
